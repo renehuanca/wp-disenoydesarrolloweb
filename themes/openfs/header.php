093e9e755a9d39bf4c28edb8eec3f4455cb3fc66
renehuanca <renehuanca999@gmail.com>
2025-06-20 16:38:28 -0400

    feat: add some styles for slider

--- themes/openfs/header.php
+++ themes/openfs/header.php
@@ -10,6 +10,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Exo:ital,wght@0,100..900;1,100..900&display=swap"
         rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&family=Roboto+Condensed:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 </head>
 
 <body>
--- themes/openfs/template-parts/hero.php
+++ themes/openfs/template-parts/hero.php
@@ -4,22 +4,24 @@
                 <source src="<?php echo get_template_directory_uri(); ?>/assets/videos/hero-video.mp4" type="video/mp4">
                 Tu navegador no soporta el video.
             </video>
+            <?php
+            $sliders = new WP_Query([
+                'post_type' => 'slide',
+                'posts_per_page' => -1,
+                'orderby' => 'menu_order',
+                'order' => 'ASC',
+            ]);
+            ?>
             <div data-aos="fade-up" class="slider-hero position-relative z-1 container">
-              <div class="frase-item active">
-                <p class="lead fs-1 text-uppercase">Soluciones en</p>
-                <h1 class="display-1 fw-bold text-uppercase">Diseño y Desarrollo Web</h1>
-              </div>
-
-              <div class="frase-item">
-                <p class="lead fs-1 text-uppercase">Especialistas en</p>
-                <h1 class="display-1 fw-bold text-uppercase">Tiendas Virtuales a Medida</h1>
-              </div>
-
-              <div class="frase-item">
-                <p class="lead fs-1 text-uppercase">Conectamos tu marca con el mundo</p>
-                <h1 class="display-1 fw-bold text-uppercase">Dominio y Hosting Profesional</h1>
-              </div>
-              <a href="diseno-y-desarrollo-web.html" class="btn btn-secondary btn-lg mt-3">SABER MÁS</a>
+              <?php if ($sliders->have_posts()): ?>
+                  <?php while($sliders->have_posts()): $sliders->the_post(); ?>
+                  <section class="frase-item" style="max-width: 800px;">
+                    <p class="fs-2 text-uppercase"><?php echo esc_html(get_field('subtitle')); ?></p>
+                    <h1 class="display-1 fw-bolder   text-uppercase"><?php the_title(); ?></h1>
+                  </section>
+                  <?php endwhile; ?>
+              <?php endif; ?>
+              <a href="#diseno-web" class="btn btn-secondary btn-lg mt-3">SABER MÁS</a>
             </div>
             <img src="<?php echo get_template_directory_uri(); ?>/assets/img/hero-bottom.svg" class="hero-bottom">
         </section>
