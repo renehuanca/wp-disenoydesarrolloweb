cb0271b058c294ccc0bc012f5f6b8bc6a188061b
renehuanca <renehuanca999@gmail.com>
2025-06-20 15:58:26 -0400

    style: adjust identation for nested blocks

--- themes/openfs/template-parts/hero.php
+++ themes/openfs/template-parts/hero.php
@@ -6,20 +6,19 @@
             </video>
             <div data-aos="fade-up" class="slider-hero position-relative z-1 container">
               <div class="frase-item active">
-  <p class="lead fs-1 text-uppercase">Soluciones en</p>
-  <h1 class="display-1 fw-bold text-uppercase">Diseño y Desarrollo Web</h1>
-</div>
+                <p class="lead fs-1 text-uppercase">Soluciones en</p>
+                <h1 class="display-1 fw-bold text-uppercase">Diseño y Desarrollo Web</h1>
+              </div>
 
-<div class="frase-item">
-  <p class="lead fs-1 text-uppercase">Especialistas en</p>
-  <h1 class="display-1 fw-bold text-uppercase">Tiendas Virtuales a Medida</h1>
-</div>
+              <div class="frase-item">
+                <p class="lead fs-1 text-uppercase">Especialistas en</p>
+                <h1 class="display-1 fw-bold text-uppercase">Tiendas Virtuales a Medida</h1>
+              </div>
 
-<div class="frase-item">
-  <p class="lead fs-1 text-uppercase">Conectamos tu marca con el mundo</p>
-  <h1 class="display-1 fw-bold text-uppercase">Dominio y Hosting Profesional</h1>
-</div>
-
+              <div class="frase-item">
+                <p class="lead fs-1 text-uppercase">Conectamos tu marca con el mundo</p>
+                <h1 class="display-1 fw-bold text-uppercase">Dominio y Hosting Profesional</h1>
+              </div>
               <a href="diseno-y-desarrollo-web.html" class="btn btn-secondary btn-lg mt-3">SABER MÁS</a>
             </div>
             <img src="<?php echo get_template_directory_uri(); ?>/assets/img/hero-bottom.svg" class="hero-bottom">
